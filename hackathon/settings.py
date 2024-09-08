e798e460ece5e73e7dfbc720d2ce64d6199a7529
Vikas <vikas1618072@gmail.com>
2024-09-08 21:06:19 +0530

    Hide secret key

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_d5*yyksj3(ve#&8+xyofi6mkvw8@((aw@e7uof6k%#zab_fn7'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
